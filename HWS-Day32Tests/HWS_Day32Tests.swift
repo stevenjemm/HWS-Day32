2bab56e0c695de7ff66d0e77ca35be4ca962ee2e
Steven Jemmott <jemmott.steven@gmail.com>
2019-11-09 22:34:17 -0400

    Initial Commit

---
+++ HWS-Day32Tests/HWS_Day32Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HWS_Day32Tests.swift
+//  HWS-Day32Tests
+//
+//  Created by Steven Jemmott on 09/11/2019.
+//  Copyright © 2019 Lagom Exp. All rights reserved.
+//
+
+import XCTest
+@testable import HWS_Day32
+
+class HWS_Day32Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
